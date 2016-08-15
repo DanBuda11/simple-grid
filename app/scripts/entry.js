5764fe909b70188e14a36f41a94c7344e96d742d
Dan Buda <danbuda@gmail.com>
2016-08-15 13:55:13 -0500

    project done

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -6,19 +6,19 @@ import ReactDOM from 'react-dom';
 const TestComponent = React.createClass({
 	render: function() {
 		return (
-			<div>
-			<div className="a1 col-1 col-2 col-3 col-4"></div>
-			<div className="a2 col-1 col-2 col-3 col-4"></div>
-			<div className="a3 col-1 col-2 col-3 col-4"></div>
-			<div className="a4 col-1 col-2 col-3 col-4"></div>
-			<div className="a5 col-1 col-2 col-3 col-4"></div>
-			<div className="a6 col-1 col-2 col-3 col-4"></div>
-			<div className="a7 col-1 col-2 col-3 col-4"></div>
-			<div className="a8 col-1 col-2 col-3 col-4"></div>
-			<div className="a9 col-1 col-2 col-3 col-4"></div>
-			<div className="a10 col-1 col-2 col-3 col-4"></div>
-			<div className="a11 col-1 col-2 col-3 col-4"></div>
-			<div className="a12 col-1 col-2 col-3 col-4"></div>
+			<div className="grid">
+			<div className="a1 col-4"></div>
+			<div className="a2 col-4"></div>
+			<div className="a3 col-4"></div>
+			<div className="a4 col-4"></div>
+			<div className="a5 col-4"></div>
+			<div className="a6 col-4"></div>
+			<div className="a7 col-4"></div>
+			<div className="a8 col-4"></div>
+			<div className="a9 col-4"></div>
+			<div className="a10 col-4"></div>
+			<div className="a11 col-4"></div>
+			<div className="a12 col-4"></div>
 			</div>
 		);
 	}
